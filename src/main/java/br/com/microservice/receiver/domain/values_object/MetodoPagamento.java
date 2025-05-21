b624821f57fbd47e4db8242613d7e248426856e9
Eduardo Rodrigues <eduardoesr@live.com>
2025-05-21 00:14:50 -0300

    Requisição

---
+++ src/main/java/br/com/microservice/receiver/domain/values_object/MetodoPagamento.java
@@ -0,0 +1,6 @@
+package br.com.microservice.receiver.domain.values_object;
+
+public enum MetodoPagamento {
+    BOLETO,
+    PIX
+}
--- src/main/java/br/com/microservice/receiver/dto/rest_controller/InputReceiverDTO.java
+++ src/main/java/br/com/microservice/receiver/dto/rest_controller/InputReceiverDTO.java
@@ -1,16 +1,19 @@
 package br.com.microservice.receiver.dto.rest_controller;
 
 import br.com.microservice.receiver.domain.values_object.Endereco;
+import br.com.microservice.receiver.domain.values_object.MetodoPagamento;
 import br.com.microservice.receiver.domain.values_object.Produto;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record InputReceiverDTO(
+        String idCliente,
+        LocalDateTime dataCriacao,
         List<Produto> produtos,
-        String cpfCliente,
-        String pagamento,
-        Endereco endereco,
-        BigDecimal frete
+        Endereco enderecoEntrega,
+        BigDecimal frete,
+        MetodoPagamento metodoPagamento
 ) {
 }
